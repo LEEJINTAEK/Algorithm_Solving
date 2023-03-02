6871db27beb3cda4593bacb38d8e8eff96cd5b4a
LEEJINTAEK <eesskk9909@gmail.com>
2023-03-02 13:18:08 +0900

    tmp검사

--- inflearn/섹션8/수들의조합.js
+++ inflearn/섹션8/수들의조합.js
@@ -18,11 +18,14 @@
 
 function solution(n, k, arr, m) {
   let answer = 0;
+  let tmp = Array.from({ length: k }, () => 0);
   function DFS(L, s, sum) {
     if (L === k) {
+      console.log(tmp);
       if (sum % m === 0) answer++;
     } else {
       for (let i = s; i < n; i++) {
+        tmp[L] = arr[i];
         DFS(L + 1, i + 1, sum + arr[i]);
       }
     }
