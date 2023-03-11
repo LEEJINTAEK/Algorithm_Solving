5aad1bceb70913b0c433e00e939e1d96ae9e5a82
LEEJINTAEK <eesskk9909@gmail.com>
2023-03-11 10:34:15 +0900

    Refactor: 코드 수정

--- programmers/LEVEL_1/카드뭉치.js
+++ programmers/LEVEL_1/카드뭉치.js
@@ -6,27 +6,11 @@ function solution1(cards1, cards2, goal) {
   let answer = "Yes";
   for (const word of goal) {
     cards1[0] === word
-      ? cards2.shift()
+      ? cards1.shift()
       : cards2[0] === word
       ? cards2.shift()
       : (answer = "No");
   }
 
   return answer;
-}
-
-//다른 풀이2
-
-function solution2(cards1, cards2, goal) {
-  for (const s of goal) {
-    if (cards1[0] === s) {
-      cards2.shift();
-    } else if (cards2[0] === s) {
-      cards2.shift();
-    } else {
-      return "no";
-    }
-  }
-
-  return "yes";
 }
