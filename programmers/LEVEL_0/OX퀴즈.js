80feabe69e6b1e992dcf5d7c051af00877da6d3a
LEEJINTAEK <eesskk9909@gmail.com>
2023-04-02 21:47:33 +0900

    Refactor: 재풀이

--- programmers/LEVEL_0/OX퀴즈.js
+++ programmers/LEVEL_0/OX퀴즈.js
@@ -1,5 +1,36 @@
 //풀이 1
 function solution(quiz) {
+  let result = [];
+  for (let i = 0; i < quiz.length; i++) {
+    let cal = [];
+    let copy = quiz[i].split(" ");
+    let answer;
+    let acc = 1;
+    for (let n of copy) {
+      if (n === "-") {
+        acc = -1;
+      }
+      if (n === "+") {
+        acc = 1;
+      }
+      if (n === "=") {
+        answer = Number(copy[copy.length - 1]);
+        break;
+      }
+      if (!isNaN(n)) {
+        cal.push(Number(n * acc));
+      }
+    }
+    cal.reduce((a, b) => a + b, 0) === answer
+      ? result.push("O")
+      : result.push("X");
+  }
+
+  return result;
+}
+
+//풀이 2
+function solution2(quiz) {
   let answer = [];
   return quiz.map((x) => {
     const [calc, result] = x.split(" = ");
@@ -10,8 +41,8 @@ function solution(quiz) {
   });
 }
 
-//풀이 2
-function solution(quiz) {
+//풀이 3
+function solution3(quiz) {
   return quiz
     .map((el) => el.split(" = "))
     .map((el) => {
--- programmers/LEVEL_0/최빈값구하기.js
+++ programmers/LEVEL_0/최빈값구하기.js
@@ -1,7 +1,37 @@
-//모름
-function solution(array) {
-  let m = new Map();
-  for (let n of array) m.set(n, (m.get(n) || 0) + 1);
-  m = [...m].sort((a, b) => b[1] - a[1]);
-  return m.length === 1 || m[0][1] > m[1][1] ? m[0][0] : -1;
+//풀이 1
+
+function solution1(array) {
+  let max = Number.MIN_SAFE_INTEGER;
+  array.sort((a, b) => a - b);
+  let answer;
+  for (let i = 0; i <= array[array.length - 1]; i++) {
+    let count = 0;
+    let tmp;
+    for (const n of array) {
+      if (i === n) {
+        count += 1;
+        tmp = n;
+      }
+    }
+    if (max === count) {
+      answer = -1;
+    }
+    if (max < count) {
+      max = count;
+      answer = tmp;
+    }
+  }
+
+  return answer;
+}
+
+//풀이 2
+function solution2(array) {
+  let map = new Map();
+  for (let n of array) {
+    map.set(n, (map.get(n) || 0) + 1);
+  }
+  map = [...map].sort((a, b) => b[1] - a[1]);
+
+  return map.length === 1 || map[0][1] > map[1][1] ? map[0][0] : -1;
 }
