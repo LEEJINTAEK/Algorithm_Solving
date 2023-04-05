ddb9f60bb3d0d7f70911a33f0d959ae912db31b9
LEEJINTAEK <eesskk9909@gmail.com>
2023-04-05 19:34:49 +0900

    Refactor: 문제 재풀이

--- programmers/LEVEL_1/약수의갯수덧셈.js
+++ programmers/LEVEL_1/약수의갯수덧셈.js
@@ -1,28 +1,26 @@
 //풀이1
-function solution1(left, right) {
-  let answer = 0;
-
+function solution(left, right) {
+  let sum = 0;
   for (let i = left; i <= right; i++) {
     let count = 0;
     for (let j = 1; j <= i; j++) {
-      if (i % j === 0) count++;
+      if (i % j === 0) {
+        count += 1;
+      }
     }
-    if (count % 2) answer -= i;
-    else answer += i;
+    count % 2 === 0 ? (sum += i) : (sum -= i);
   }
 
-  return answer;
+  return sum;
 }
 
 //풀이2
-function solution2(left, right) {
-  let answer = 0;
+function solution(left, right) {
+  let sum = 0;
   for (let i = left; i <= right; i++) {
-    if (Number.isInteger(Math.sqrt(i))) {
-      answer -= i;
-    } else {
-      answer += i;
-    }
+    //제곱근이 정수이면 약수가 홀수
+    Number.isInteger(Math.sqrt(i)) ? (sum -= i) : (sum += i);
   }
-  return answer;
+
+  return sum;
 }
