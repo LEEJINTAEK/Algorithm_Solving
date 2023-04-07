627d434065f75700fa45fa1daceb6b3f1a3f90b7
LEEJINTAEK <eesskk9909@gmail.com>
2023-04-07 21:43:57 +0900

    Refactor: 재풀이

--- programmers/LEVEL_1/공약수공배수.js
+++ programmers/LEVEL_1/공약수공배수.js
@@ -1,23 +1,17 @@
 //풀이 1
-function solution1(n, m) {
-  let max = 0;
-  let min = 0;
+function solution(n, m) {
+  let max;
+  let min;
   for (let i = 1; i <= Math.min(n, m); i++) {
     if (n % i === 0 && m % i === 0) {
       max = i;
     }
   }
-  for (let i = 2; i <= n * m; i++) {
+  for (let i = Math.max(n, m); i <= n * m; i++) {
     if (i % n === 0 && i % m === 0) {
       min = i;
+
       return [max, min];
     }
   }
-}
-
-//풀이 2
-function solution2(a, b) {
-  var r;
-  for (var ab = a * b; (r = a % b); a = b, b = r) {}
-  return [b, ab / b];
 }
--- programmers/LEVEL_1/예산.js
+++ programmers/LEVEL_1/예산.js
@@ -1,13 +1,31 @@
-//풀이
-function solution1(d, budget) {
+//풀이 1
+function solution(d, budget) {
+  let sum = 0;
+  let count = 0;
+  d.sort((a, b) => a - b);
+  for (const money of d) {
+    if (sum < budget) {
+      sum += money;
+      count += 1;
+    }
+    if (sum > budget) {
+      return count - 1;
+    }
+  }
+
+  return count;
+}
+
+//풀이2
+function solution2(d, budget) {
   return d
     .sort((a, b) => a - b)
     .map((x) => (x <= budget ? (budget = budget - x) : false))
     .filter((x) => x !== false).length;
 }
 
-//풀이2
-function solution2(d, budget) {
+//풀이3
+function solution3(d, budget) {
   d.sort((a, b) => a - b);
 
   while (d.reduce((a, b) => a + b, 0) > budget) d.pop();
