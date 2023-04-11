fe6448a3ee5b24ef09834ccb84d4381098fa702e
LEEJINTAEK <eesskk9909@gmail.com>
2023-04-11 09:48:16 +0900

    Refactor: 재풀이

--- programmers/LEVEL_1/콜라문제.js
+++ programmers/LEVEL_1/콜라문제.js
@@ -1,8 +1,9 @@
 function solution(a, b, n) {
-    let cola = 0;
-    while(n>=a){
-        cola = cola + parseInt(n/a) * b;
-        n = parseInt(n/a)*b + parseInt(n%a);
-    }
-    return cola;
+  let colaSave = 0;
+  while (n >= a) {
+    colaSave = colaSave + parseInt(n / a) * b;
+    n = parseInt(n / a) * b + parseInt(n % a);
+  }
+
+  return colaSave;
 }
