0141ca848be55026ebbb1fc4daf0b84ef3b25457
LEEJINTAEK <eesskk9909@gmail.com>
2023-04-12 11:30:28 +0900

    Refactor: 재풀이

--- programmers/LEVEL_1/모의고사.js
+++ programmers/LEVEL_1/모의고사.js
@@ -1,8 +1,8 @@
 //풀이 1
 function solution(answers) {
-  let person1 = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
-  let person2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  let person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const person1 = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+  const person2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
   let right = [0, 0, 0];
   //정답 맞을 때 right 증가
   for (let i = 0; i < answers.length; i++) {
@@ -23,15 +23,15 @@ function solution(answers) {
 //풀이 2
 
 function solution(answers) {
-  var answer = [];
-  var a1 = [1, 2, 3, 4, 5];
-  var a2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  var a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let answer = [];
+  const a1 = [1, 2, 3, 4, 5];
+  const a2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-  var a1c = answers.filter((a, i) => a === a1[i % a1.length]).length;
-  var a2c = answers.filter((a, i) => a === a2[i % a2.length]).length;
-  var a3c = answers.filter((a, i) => a === a3[i % a3.length]).length;
-  var max = Math.max(a1c, a2c, a3c);
+  const a1c = answers.filter((a, i) => a === a1[i % a1.length]).length;
+  const a2c = answers.filter((a, i) => a === a2[i % a2.length]).length;
+  const a3c = answers.filter((a, i) => a === a3[i % a3.length]).length;
+  const max = Math.max(a1c, a2c, a3c);
 
   if (a1c === max) {
     answer.push(1);
