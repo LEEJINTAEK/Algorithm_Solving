2b4423caa108767e54f4c4f30c70b1cb9088e82d
LEEJINTAEK <eesskk9909@gmail.com>
2023-04-13 13:15:41 +0900

    Refactor:재풀이

--- programmers/LEVEL_1/소수찾기.js
+++ programmers/LEVEL_1/소수찾기.js
@@ -1,21 +1,21 @@
-// 에라토스테네스의 체
-
-// 주어진 숫자가 101일때 2부터 101의 제곱근(10) 까지 차례대로 각각의 배수들을 모두 삭제 시키고 남은 숫자가 소수(2~101사이의)이다.
 //풀이 1
+// 에라토스테네스의 체
+// 주어진 숫자가 101일때 2부터 101의 제곱근(10) 까지 차례대로 각각의 배수들을 모두 삭제 시키고 남은 숫자가 소수(2~101사이의)이다.
 function solution1(n) {
-  let arr = Array(n + 1)
+  let arr = Array.from({ length: n + 1 })
     .fill(true)
     .fill(false, 0, 2);
-  console.log(arr);
-  for (let i = 2; i * i <= n; i++) {
+  for (let i = 2; Math.pow(i, 2) <= n; i++) {
     if (arr[i]) {
-      for (let j = i * i; j <= n; j += i) {
+      for (let j = Math.pow(i, 2); j <= n; j += i) {
         arr[j] = false;
       }
     }
   }
-  return arr.filter((item) => item === true).length;
+
+  return arr.filter((num) => num === true).length;
 }
+
 //풀이 2
 function solution2(n) {
   const s = new Set();
@@ -31,5 +31,6 @@ function solution2(n) {
       }
     }
   }
+
   return s.size;
 }
