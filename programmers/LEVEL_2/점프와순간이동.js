9ef36e655c72ccd3c8caaa8c02b5f98cc909583f
LEEJINTAEK <eesskk9909@gmail.com>
2023-06-07 10:30:49 +0900

    feat: 문제추가

---
+++ programmers/LEVEL_2/점프와순간이동.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  let battery = 0;
+
+  while (n > 0) {
+    if (n % 2 === 0) {
+      n /= 2;
+    } else {
+      battery += 1;
+      n -= 1;
+    }
+  }
+
+  return battery;
+}
--- programmers/LEVEL_2/행렬의곱셈.js
+++ programmers/LEVEL_2/행렬의곱셈.js
@@ -1,4 +1,10 @@
-function solution(arr1, arr2) {
+function solution1(arr1, arr2) {
+  return arr1.map((row) =>
+    arr2[0].map((x, y) => row.reduce((a, b, c) => a + b * arr2[c][y], 0))
+  );
+}
+
+function solution2(arr1, arr2) {
   const row1 = arr1.length;
   const col1 = arr1[0].length;
   const col2 = arr2[0].length;
