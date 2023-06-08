529504ee50ab6dc46d18688194c5490d81697d93
LEEJINTAEK <eesskk9909@gmail.com>
2023-06-08 11:42:20 +0900

    feat: 문제 추가

---
+++ programmers/LEVEL_2/의상.js
@@ -0,0 +1,12 @@
+function solution(clothes) {
+  let map = new Map();
+  for (const x of clothes) {
+    map.has(x[1]) ? map.set(x[1], map.get(x[1]) + 1) : map.set(x[1], 1);
+  }
+  let cal = 1;
+  for (const value of map.values()) {
+    cal = cal * (value + 1);
+  }
+
+  return cal - 1;
+}
