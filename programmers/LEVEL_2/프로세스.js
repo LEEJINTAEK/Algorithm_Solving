84468f62dcc02f69284aa13225ff089a0f7ab954
LEEJINTAEK <eesskk9909@gmail.com>
2023-06-12 12:16:49 +0900

    feat: 프로세스 문제

---
+++ programmers/LEVEL_2/프로세스.js
@@ -0,0 +1,16 @@
+function solution(priorities, location) {
+  let prior = priorities.slice().sort((a, b) => b - a);
+  let cnt = 1;
+  let process = prior.shift();
+  while (1) {
+    for (let i = 0; i < priorities.length; i++) {
+      if (priorities[i] === process) {
+        if (i === location) {
+          return cnt;
+        }
+        process = prior.shift();
+        cnt += 1;
+      }
+    }
+  }
+}
