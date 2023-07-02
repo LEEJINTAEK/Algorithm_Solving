7f5b7368ece395718db4b53b0c02fc7bd87a5f44
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-07-02 13:50:26 +0900

    faet: 문제추가

---
+++ programmers/LEVEL_2/숫자변환하기.js
@@ -0,0 +1,72 @@
+//BFS
+function solution1(x, y, n) {
+  if (x === y) {
+    return 0;
+  }
+  let q = [[x, 0]];
+  let visited = new Set();
+
+  let front = 0;
+  let rear = 1;
+
+  while (front < rear) {
+    let [cur, cnt] = q[front++];
+    let nextNumber;
+
+    if (cur === y) {
+      return cnt;
+    }
+    if (cur > y) {
+      continue;
+    }
+
+    nextNumber = cur + n;
+    if (!visited.has(nextNumber)) {
+      visited.add(nextNumber);
+      q[rear++] = [nextNumber, cnt + 1];
+    }
+
+    nextNumber = cur * 2;
+    if (!visited.has(nextNumber)) {
+      visited.add(nextNumber);
+      q[rear++] = [nextNumber, cnt + 1];
+    }
+
+    nextNumber = cur * 3;
+    if (!visited.has(nextNumber)) {
+      visited.add(nextNumber);
+      q[rear++] = [nextNumber, cnt + 1];
+    }
+  }
+  return -1;
+}
+
+//DP
+function solution2(x, y, n) {
+  if (x === y) {
+    return 0;
+  }
+
+  let dp = Array(y + 1).fill(Infinity);
+  dp[x] = 0;
+
+  for (let i = x; i <= y; i++) {
+    if (dp[i] === Infinity) {
+      continue;
+    }
+
+    if (i + n <= y) {
+      dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
+    }
+
+    if (i * 2 <= y) {
+      dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
+    }
+
+    if (i * 3 <= y) {
+      dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
+    }
+  }
+
+  return dp[y] === Infinity ? -1 : dp[y];
+}
