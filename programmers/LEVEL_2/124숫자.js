d92a34ee72bce9b4fa629267b413eaf42a0de670
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-08-09 17:49:50 +0900

    124 나라의 숫자

---
+++ programmers/LEVEL_2/124숫자.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  const answer = [];
+  while (n > 0) {
+    if (n % 3 === 0) {
+      answer.push(4);
+      n = Math.floor(n / 3) - 1;
+    } else {
+      answer.push(n % 3);
+      n = Math.floor(n / 3);
+    }
+  }
+
+  return answer.reverse().join("");
+}
