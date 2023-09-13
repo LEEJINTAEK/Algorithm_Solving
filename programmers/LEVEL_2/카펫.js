10056c2f52870df6377096a0987e898bc37e64dc
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-09-13 15:38:34 +0900

    완전탐색

--- programmers/LEVEL_2/카펫.js
+++ programmers/LEVEL_2/카펫.js
@@ -1,12 +1,25 @@
 function solution(brown, yellow) {
-  let area = brown + yellow;
-
-  for (let i = parseInt(Math.sqrt(area)); i >= 3; i--) {
-    if (area % i === 0) {
-      let j = area / i;
-      if ((i - 2) * (j - 2) === yellow) {
-        return i >= j ? [i, j] : [j, i];
+  for (let w = 1; w <= yellow; w++) {
+    if (yellow % w === 0) {
+      const h = yellow / w;
+      const totalBrown = (w + 2) * 2 + h * 2;
+      if (totalBrown === brown) {
+        return [h + 2, w + 2];
       }
     }
   }
+  return [];
 }
+
+// function solution(brown, yellow) {
+//   let area = brown + yellow;
+
+//   for (let i = parseInt(Math.sqrt(area)); i >= 3; i--) {
+//     if (area % i === 0) {
+//       let j = area / i;
+//       if ((i - 2) * (j - 2) === yellow) {
+//         return i >= j ? [i, j] : [j, i];
+//       }
+//     }
+//   }
+// }
