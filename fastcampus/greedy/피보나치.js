8f1091c2abb6c85328feeadfac764c9b6a2d3337
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2024-01-01 17:42:31 +0900

    solved: 그리디 백준

---
+++ fastcampus/greedy/피보나치.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const pibo = [0, 1];
+while (pibo[pibo.length - 1] < 1e9) {
+  pibo.push(pibo[pibo.length - 1] + pibo[pibo.length - 2]);
+}
+
+for (let tc = 1; tc <= input[0]; tc++) {
+  const num = Number(input[tc]);
+  let i = pibo.length - 1;
+  const answer = [];
+  while (num > 0) {
+    if (num >= pibo[i]) {
+      num -= pibo[i];
+      answer.push(pibo[i]);
+    }
+    i -= 1;
+  }
+
+  answer.sort((a, b) => a - b);
+  console.log(answer.join(" "));
+}
