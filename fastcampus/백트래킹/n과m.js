e53181018c1061ced265a0ea058f7ffb6bce4208
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2024-01-21 17:37:58 +0900

    solved: 재풀이 후 리펙토링

--- fastcampus/백트래킹/n과m.js
+++ fastcampus/백트래킹/n과m.js
@@ -1,30 +1,64 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n
-');
-let [n, m] = input[0].split(" ").map(Number); // 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
-let arr = []; // 순열을 계산하고자 하는 원소(element)가 담긴 배열
-for (let i = 1; i <= n; i++) arr.push(i);
-let visited = new Array(n).fill(false); // 각 원소 인덱스(index)별 방문 여부
-let selected = []; // 현재 순열에 포함된 원소(element)의 인덱스
-let answer = 
-"";
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+// const input = ["4 2"];
+const [n, m] = input[0].split(" ").map(Number);
+
+const arr = [];
+for (let i = 1; i <= n; i++) {
+  arr.push(i);
+}
+const visited = new Array(n).fill(false);
+const selected = [];
+const answer = "";
 function dfs(arr, depth) {
-if (depth == m) { // 모든 순열을 확인하는 부분
-let result = []; // 순열(permutation) 결과 저장 테이블
-for (let i of selected) result.push(arr[i]);
-for (let x of result) answer += x + " "; // 계산된 순열을 실질적으로 처리하는 부분
-answer += "\n
-";
-return;
-}
-for (let i = 0; i < arr.length; i++) { // 하나씩 원소 인덱스(index)를 확인하며
-if (visited[i]) continue; // [중복을 허용하지 않으므로] 이미 처리 된 원소라면 무시
-selected.push(i); // 현재 원소 선택
-visited[i] = true; // 현재 원소 방문 처리
-dfs(arr, depth + 1); // 재귀 함수 호출
-selected.pop(); // 현재 원소 선택 취소
-visited[i] = false; // 현재 원소 방문 처리 취소
-}
+  if (depth == m) {
+    const result = [];
+    for (let i of selected) {
+      result.push(arr[i]);
+    }
+    for (let x of result) {
+      answer += x + " ";
+    }
+    answer += "\n";
+    return;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (visited[i]) {
+      continue;
+    }
+    selected.push(i);
+    visited[i] = true;
+    dfs(arr, depth + 1);
+    selected.pop();
+    visited[i] = false;
+  }
 }
 dfs(arr, 0);
-console.log(answer)
+console.log(answer);
+
+// const answer = [];
+// const check = Array.from({ length: n + 1 }, () => false);
+
+// function dfs(depth, arr) {
+//   if (depth > n || arr.length > m) {
+//     return;
+//   }
+//   if (arr.length === m) {
+//     answer.push(arr);
+//     return;
+//   }
+//   for (let i = 1; i <= n; i++) {
+//     if (!check[i]) {
+//       check[i] = true;
+//       dfs(depth, [...arr, i]);
+//       check[i] = false;
+//     }
+//   }
+// }
+// for (let d = 1; d <= n; d++) {
+//   check[d] = true;
+//   dfs(d, [d]);
+//   check[d] = false;
+// }
+
+// console.log(answer.map((num) => num.join(" ")).join("\n"));
