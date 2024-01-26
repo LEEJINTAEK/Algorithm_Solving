36e999e17e0076adb96c5425cdfd585b3fe9854a
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2024-01-27 08:11:59 +0900

    solved: 백트래킹 풀이 추가

--- fastcampus/백트래킹/n과m4.js
+++ fastcampus/백트래킹/n과m4.js
@@ -21,3 +21,29 @@ for (let i = 1; i <= n; i++) {
   dfs(i, [i]);
 }
 console.log(answer.join("\n"));
+
+// 백트래킹
+
+const [n, m] = input[0].split(" ").map(Number); // 1부터 N까지 자연수 중에서 M개를 고른 중복 조합
+let arr = []; // 중복 조합을 계산하고자 하는 원소(element)가 담긴 배열
+for (let i = 1; i <= n; i++) arr.push(i);
+let selected = []; // 현재 중복 조합에 포함된 원소(element)의 인덱스
+let answer = "";
+function dfs(arr, depth, start) {
+  if (depth == m) {
+    // 모든 중복 조합을 확인하는 부분
+    let result = []; // 중복 조합 결과 저장 테이블
+    for (let i of selected) result.push(arr[i]); // 계산된 중복 조합을 실질적으로 처리하는 부분
+    for (let x of result) answer += x + " ";
+    answer += "\n";
+    return;
+  }
+  for (let i = start; i < arr.length; i++) {
+    // start 지점부터 하나씩 원소 인덱스(index)를 확인하며
+    selected.push(i); // 현재 원소 선택
+    dfs(arr, depth + 1, i); // 조합이므로, 재귀 함수 호출시 현재 인덱스(index)를 넣기
+    selected.pop(); // 현재 원소 선택 취소
+  }
+}
+dfs(arr, 0, 0);
+console.log(answer);
