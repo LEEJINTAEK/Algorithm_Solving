bff6b834e3336a0d61ed7b01216dd7fedbc37d09
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2024-03-19 10:34:16 +0900

    refactor: 사용하지않는 변수 제거

--- programmers/LEVEL_2/이모티콘할인행사.js
+++ programmers/LEVEL_2/이모티콘할인행사.js
@@ -1,6 +1,4 @@
 function solution(users, emoticons) {
-  let [totalMemberShip, totalSales] = [0, 0];
-
   return buy(users, emoticons);
 }
 
@@ -56,18 +54,3 @@ function buy(users, emoticons) {
 
   return answer[0];
 }
-
-console.log(
-  solution(
-    [
-      [40, 2900],
-      [23, 10000],
-      [11, 5200],
-      [5, 5900],
-      [40, 3100],
-      [27, 9200],
-      [32, 6900],
-    ],
-    [1300, 1500, 1600, 4900]
-  )
-);
